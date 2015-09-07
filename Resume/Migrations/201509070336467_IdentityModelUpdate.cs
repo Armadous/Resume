32fb9536cdde9ff974eb247b6d65624bdc1d4217
Joshua Slaven <joshuaslaven42@gmail.com>
2015-09-06 23:53:22 -0400

    Added IdentityDbContext to code first migrations. .NET updated the underlying IdentityUser class necessitating a schema update.

---
+++ Resume/Migrations/201509070336467_IdentityModelUpdate.cs
@@ -0,0 +1,16 @@
+namespace Resume.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IdentityModelUpdate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
