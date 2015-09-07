1b8f40eb53ce8e7872a58148142d63b621698b19
Joshua Slaven <joshuaslaven42@gmail.com>
2015-09-07 03:57:52 -0400

    Temporary method to output headers to troubleshoot appharbor RequireHttps

--- Resume/Controllers/HomeController.cs
+++ Resume/Controllers/HomeController.cs
@@ -8,6 +8,17 @@ namespace Resume.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Headers()
+        {
+            var headers = "";
+            foreach(var header in Request.Headers.Keys)
+            {
+                headers += string.Format("{0}:{1},", header, Request.Headers[header.ToString()]);
+            }
+
+            return Content(headers);
+        }
+
         public ActionResult Index()
         {
             return View();
