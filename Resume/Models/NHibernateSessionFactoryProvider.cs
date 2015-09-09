a6fd6fc0e4ff19bac25aefa0c2f3bff060dc27e0
Joshua Slaven <joshuaslaven42@gmail.com>
2015-09-09 01:08:31 -0400

    Add Fluent NHibernate and ninject to replace EntityFramework data persistance layer. App WILL sync NHibernate schema on app starter now

---
+++ Resume/Models/NHibernateSessionFactoryProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+using Ninject.Activation;
+using Ninject;
+using Ninject.Modules;
+using Ninject.Web.Common;
+
+namespace Resume.Models
+{
+    public class NHibernateSessionFactoryProvider : Provider<ISessionFactory>
+    {
+        protected override ISessionFactory CreateInstance(IContext context)
+        {
+            var sessionFactory = new NHibernateSessionFactory();
+            return sessionFactory.GetSessionFactory();
+        }
+    }
+
+    public class NHibernateModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ISessionFactory>().ToProvider<NHibernateSessionFactoryProvider>().InSingletonScope();
+            Bind<ISession>().ToMethod(context => context.Kernel.Get<ISessionFactory>().OpenSession()).InRequestScope();
+        }
+    }
+}
---
+++ Resume/Models/ResumeService.cs
@@ -0,0 +1,17 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Resume.Models
+{
+    public class ResumeService
+    {
+        private readonly ISession session;
+        public ResumeService(ISession session)
+        {
+            this.session = session;
+        }
+    }
+}
